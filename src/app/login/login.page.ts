434388d211694812ea8278e5014d0b6168b303b2
Carl Xyreuss Adaya <cxyreussadaya@gmail.com>
2023-07-17 23:24:28 +0800

    4th

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, AlertController } from '@ionic/angular';
+import { PasswordService } from '../PassService/pass.service';
 
 @Component({
   selector: 'app-login',
@@ -10,26 +11,42 @@ export class LoginPage implements OnInit {
   username!: string;
   password!: string;
 
-constructor(
-  private navCtrl: NavController, 
-  private alertController: AlertController
+  constructor(
+    private navCtrl: NavController, 
+    private passwordService: PasswordService,
+    private alertController: AlertController
   ) {}
 
   ngOnInit() {
   }
+
   redirectToHome() {
     this.navCtrl.navigateRoot('/home');
   }
-  async login() {
-    if (this.username === 'admin' && this.password === 'admin') {
-      this.navCtrl.navigateForward('/home');
+
+  async showAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Invalid Credentials',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  login() {
+    // Get the password from the password service
+    const storedPassword = this.passwordService.getPassword();
+
+    // Validate the username and password
+    if (this.username === 'admin' && this.password === storedPassword) {
+      // Handle successful login
+      console.log('Login successful');
+      this.navCtrl.navigateRoot('/home');
     } else {
-      const alert = await this.alertController.create({
-        header: 'Login Failed',
-        message: 'Invalid credentials',
-        buttons: ['try again']
-      });
-      await alert.present();
+      // Handle invalid credentials
+      console.log('Invalid credentials');
+      this.showAlert('The password you entered is incorrect.');
     }
   }
 }
